17b0cce2f2262e626c38c55558a5da7e510c950b
Rishi Hancock <riha2762@colorado.edu>
2024-10-24 18:31:12 -0600

    fix

--- teleop_deeprace_pkg/setup.py
+++ teleop_deeprace_pkg/setup.py
@@ -6,7 +6,6 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    pymodules=['teleoop_deepracer_node'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
