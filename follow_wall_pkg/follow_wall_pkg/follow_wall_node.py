11923fc1adc425e86eba46da9ac88f628d0ad193
Mohammed Al Alawi <moal3965@colorado.edu>
2024-12-14 18:41:55 -0700

    left turnning follow wall

--- follow_wall_pkg/follow_wall_pkg/follow_wall_node.py
+++ follow_wall_pkg/follow_wall_pkg/follow_wall_node.py
@@ -24,37 +24,13 @@ class FollowWallNode(Node):
             10
         )
         # always keep the mobile 1.0 meters away from right wall
-        self.desired_distance_right = 1.0 #initially
+        self.desired_distance_right = 1.0 
 
-        self.angle_pid = PID(-0.7,0.0,0.0,setpoint = 0.0)
-        self.angle_pid.output_limits = (-1.0,1.0)
+        #self.angle_pid = PID(-0.7,0.0,0.0,setpoint = 0.0)
+        #self.angle_pid.output_limits = (-1.0,1.0)
 
-        self.stop = False
-        self.moving = True
         self.reverse_run = False
 
-       # threading.Thread(target = self.listen_for_keyboard_input,daemon = True).start()
-    def get_key(self):
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(fd)
-            key = sys.stdin.read(1)  # Read 1 character first
-            if key == '\x1b':  # If the first character is an escape character
-                key += sys.stdin.read(2)  # Read the next 2 characters (for arrow keys)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return key
-
-    def listen_for_keyboard_input(self):
-        while True:
-            key = self.get_key()
-            if key == 's':
-                self.stop = True
-                self.get_logger().info("robot stop")
-            elif key == 'r':
-                self.stop = False
-                self.get_logger().info("resumed")
 
     def lidar_callback(self, msg: LaserScan):
         # Extract the LIDAR scan ranges
@@ -77,40 +53,31 @@ class FollowWallNode(Node):
 
         # Check for obstacles within the goal direction
         #Follow wall on the right
-        dist_right = dist_means[6]
-        #dist_left = dist_means[1]
+        #dist_right = dist_means[6]
+        dist_left = dist_means[1]
+         
+        error_left = self.desired_distance_right - dist_left
+        self.run(error_left)
 
-        #self.desired_distance_right = (dist_right+dist_left)/2
-        #if (dist_right > self.desired_distance_right*0.8) and (dist_right < self.desired_distance_right*1.2):
-            #self.desired_distance_right = dist_right         
-        error_right = self.desired_distance_right - dist_right 
-        self.run(error_right)
+    def run(self,error_left):
+        base_throttle = 0
+        Kp = -0.5
+        angle_corrected = Kp*error_left
+        if angle_corrected > 1:
+            angle_corrected = 1
+        if angle_corrected < -1:
+            angle_corrected = -1
 
-    def run(self,error_right):
-        self.stop = False
-        if self.stop:
-            self.get_logger.info("stop")
-            wheel_msg = ServoCtrlMsg()
-            wheel_msg.throttle = 0.0
-            wheel_msg.angle = 0.0
-            self.wheel_publisher.publish(wheel_msg)
-            return
-        else:
-
-            base_throttle = 0.6
-            angle_corrected = self.angle_pid(error_right)
-            # throttle_corrected = base_throttle + self.throttle_pid(abs(error_right))
-
-            if self.reverse_run:
-                throttle_input *= -1
-                angle_corrected *= -1
-            # Populate the control message
-            wheel_msg = ServoCtrlMsg()
-            wheel_msg.throttle = base_throttle
-            wheel_msg.angle = angle_corrected
-            # Publish the control message
-            self.get_logger().info(f"Error: {error_right:.2f}, Throttle: {wheel_msg.throttle:.2f}, Angle: {wheel_msg.angle:.2f}")
-            self.wheel_publisher.publish(wheel_msg)
+        if self.reverse_run:
+            throttle_input *= -1
+            angle_corrected *= -1
+        # Populate the control message
+        wheel_msg = ServoCtrlMsg()
+        wheel_msg.throttle = base_throttle
+        wheel_msg.angle = angle_corrected
+        # Publish the control message
+        self.get_logger().info(f"Error: {error_right:.2f}, Throttle: {wheel_msg.throttle:.2f}, Angle: {wheel_msg.angle:.2f}")
+        self.wheel_publisher.publish(wheel_msg)
 
 def main(args=None):
     rclpy.init(args=args)
